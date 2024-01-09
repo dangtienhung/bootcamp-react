25d2e149fc0222ea92becb193e7a8c80f84dac1b
cô giáo hưng <hungdang02042003@gmail.com>
2024-01-09 22:05:43 +0700

    buổi 1: ôn tập useState

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,32 @@ import { useState } from 'react';
 function App() {
 	// useState is a React Hook
 	// useState dùng để quản lý trạng thái state của component
-	const arrayName = ['Huy', 'Hùng', 'Hà', 'Hưng', 'Hải'];
+	const arrayName = ['Huy', 'Hùng', 'Hà', 'Hưng', 'Hải', 'Hà'];
+
+	const [data, setData] = useState(arrayName);
+	const handleClick = (name) => {
+		const newDate = data.filter((nameItem) => {
+			if (name !== nameItem) {
+				return nameItem;
+			}
+		});
+		setData(newDate);
+	};
 
 	return (
-		<div>
-			{arrayName.map((name, index) => {
-				return <div key={index}>{name}</div>;
+		<div className="flex flex-col gap-4">
+			{data.map((name, index) => {
+				return (
+					<div key={index} className="flex gap-4 items-center">
+						<p>{name}</p>
+						<button
+							onClick={() => handleClick(name)}
+							className="border py-2 px-4 border-red-500 rounded"
+						>
+							Delete
+						</button>
+					</div>
+				);
 			})}
 		</div>
 	);
